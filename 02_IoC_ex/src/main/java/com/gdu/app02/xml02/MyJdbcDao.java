fd1ce5271b4ead9a449d1f8a002801e48625c3f4
bvbvbv42 <bvmn2@naver.com>
2023-09-25 17:23:44 +0900

    20230925

--- 02_IoC_ex/src/main/java/com/gdu/app02/xml02/MyJdbcDao.java
+++ 02_IoC_ex/src/main/java/com/gdu/app02/xml02/MyJdbcDao.java
@@ -6,33 +6,51 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MyJdbcDao {
-
+  
   private Connection con;
+  private AbstractApplicationContext ctx;
+  private MyJdbcConnection myJdbcConnection;
   
-  public Connection getConnection() {
-    // 반환값 : MyJdbcConnction 클래스의 getConnection() 메소드를 호출
-    AbstractApplicationContext ctx = new GenericXmlApplicationContext("xml02/appCtx.xml");
-    MyJdbcConnection myJdbcConnection = ctx.getBean("myJdbcConnection", MyJdbcConnection.)
+  private Connection getConnection() {
+    ctx = new GenericXmlApplicationContext("xml02/appCtx.xml");
+    myJdbcConnection = ctx.getBean("myJdbcConnection", MyJdbcConnection.class);
     ctx.close();
-    return MyJdbcConnection.getConnection();
+    return myJdbcConnection.getConnection();
+  }
   
+  private void close() {
+    try {
+      if(con != null) {
+        con.close();
+        System.out.println(myJdbcConnection.getUser() + " 접속 해제");
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
   
   public void add() {
-    
-    
+    con = getConnection();
+    System.out.println("add() 호출");
+    close();
   }
   
   public void remove() {
-    
-    
+    con = getConnection();
+    System.out.println("remove() 호출");
+    close();
   }
+  
   public void modify() {
-    
-    
+    con = getConnection();
+    System.out.println("modify() 호출");
+    close();
   }
-  public void delete() {
-    
-    
+  
+  public void select() {
+    con = getConnection();
+    System.out.println("select() 호출");
+    close();
   }
+  
 }
