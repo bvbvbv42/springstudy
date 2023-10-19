b50e58618a479b9238c58531ed6dc689ee7b3a9b
bvbvbv42 <bvmn2@naver.com>
2023-10-19 12:35:26 +0900

    20231019

---
+++ 15_myhome/src/main/java/com/gdu/myhome/util/MySecurityUtils.java
@@ -0,0 +1,12 @@
+package com.gdu.myhome.util;
+
+public class MySecurityUtils {
+
+  /*
+   * SHA256 암호화
+   * 1. 입력값을 256비트(32바이트)로 암호화하는 해시 알고리즘이다. 
+   * 2. 원본을 암호화할 수 있으나, 암호화된 결과를 원본으로 되돌리는 복호화는 불가능하다.
+   * 3. java.security 패키지를 활용해서 구하거나, 
+   */
+  
+}
