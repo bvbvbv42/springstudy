aa391f85037916db016a0ce4108812b9e9500413
bvbvbv42 <bvmn2@naver.com>
2023-10-27 14:29:43 +0900

    20231027

---
+++ 15_myhome/src/main/java/com/gdu/myhome/controller/BlogController.java
@@ -0,0 +1,56 @@
+package com.gdu.myhome.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.gdu.myhome.service.BlogService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/blog")
+@RequiredArgsConstructor
+@Controller
+public class BlogController {
+
+  private final BlogService blogService;
+  
+  @GetMapping("/list.do")
+  public String list(HttpServletRequest request, Model model) {
+    return "blog/list";
+  }
+  
+  @GetMapping("/write.form")
+  public String write() {
+    return "blog/write";
+  }
+  
+  @ResponseBody  //produces 랑 세트!
+  @PostMapping(value="/imageUpload.do", produces="application/json")
+  // 이미지는 postmapping
+  
+  public Map<String, Object> imageUpload(MultipartHttpServletRequest multipartRequest) {
+    // ㄴ> 곧바로 서비스로넘김
+    return blogService.imageUpload(multipartRequest);
+  }
+  
+  @PostMapping("/addBlog.do")
+  public String addBlog(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    int addResult = blogService.addBlog(request);
+    redirectAttributes.addFlashAttribute("addResult", addResult);
+    return "redirect:/blog/list.do";
+  }
+  
+  
+  
+  
+}
